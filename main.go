dbcb4f5e422444495e761a4d19bdfb2fe076698c
ittiphatch <ittiphat.ch@gmail.com>
2024-04-18 02:47:58 +0700

    Add cors section for using with vue

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	models_practice "go-project/models"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
-
 func main() {
-
+	// Connect to database
 	models_practice.InitialDB()
 
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"http://localhost:5173"},
+		AllowMethods: []string{"GET", "POST", "PUT", "DELETE"},
+		AllowCredentials: true,
+	}))
 	router.GET("/food", models_practice.GetFood)
 	router.POST("/food", models_practice.PostFood)
 	router.GET("/food/:id", models_practice.GetFoodByID)
